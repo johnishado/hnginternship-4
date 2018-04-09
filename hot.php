41b33e8c696674dba5476ab041a04ba765bd12b0
ishadojohn <johniashibekong@gmail.com>
2018-04-09 14:15:27 -0800

    first commit

---
+++ hot.php
@@ -0,0 +1,26 @@
+<html>
+        <head>
+            <title>
+                my hotel.ng first task
+            </title> 
+            <link rel="stylesheet" type="text/css" href="ctest.css"/> 
+        </head>
+        <body>
+            <strong><center> HNG INTERNSHIP 4</center></strong>
+            <div id="figma">
+            <P><center> <strong>MY FIRST FIGMA PROJECT</strong></center></P>
+            </div>
+           <center> <P><strong> THIS SHOULD BE INTERESTING</strong></P></center>
+            <div class="time">
+<?php
+echo "THE TIME IS " . date("h:i:sa");
+
+
+?>                
+
+            </div>
+            <div id="down">
+                <center><strong> ASHIBEKONG JOHN ISHADO</strong></center>
+            </div>
+    </body>
+</html>
